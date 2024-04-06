08d5d75bc00d5cd7c382b7ee18b64bb7f35d5900
abelcarriizo <abelcarriizo@outlook.com>
2024-04-06 16:56:38 -0300

    clase "Main" añadidad

---
+++ ejercicio_0/Main.java
@@ -0,0 +1,10 @@
+package ejercicio_0;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        Caja caja1 = new Caja(3,2,6);
+        int resultado = caja1.calculadoraVolumen();
+        System.out.println(resultado);
+    }
+}
